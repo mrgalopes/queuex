35c0a80c60633fd646f83e92c276364b5d98d512
Gabriel Lopes Rodrigues <gabriellopes@usp.br>
2022-10-03 16:47:43 -0300

    Initial commit

---
+++ lib/queuex.ex
@@ -0,0 +1,18 @@
+defmodule Queuex do
+  @moduledoc """
+  Documentation for `Queuex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Queuex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
